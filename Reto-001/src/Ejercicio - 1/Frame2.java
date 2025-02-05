59b447efe0b58aba25d0f9fa567ef8a96af26b10
JP <144849667+JPMoralesl004@users.noreply.github.com>
2025-02-05 13:29:41 +0100

    Update Frame2.java

--- Reto-001/src/Ejercicio - 1/Frame2.java
+++ Reto-001/src/Ejercicio - 1/Frame2.java
@@ -1,11 +1,24 @@
-class Frame2 {
-    private char[][] contenido;
-    
-    public Frame2(int width, int height) {
-        contenido = new char[height][width];
+public class Frame2 {
+    char[][] contenido;
+
+    public Frame2(int ancho, int alto) {
+        contenido = new char[][] {
+                { '.', '.', '.', '#', '#', '.', '.' },
+                { '+', '+', '.', '.', '.', '.', '.' },
+                { '#', '#', '#', '.', '.', '.', '.' }
+        };
     }
-    
-    public char[][] getContenido() {
-        return contenido;
+
+    public void posicionFrame(Pixel pixel) {
+        contenido = pixel.listaElementos();
+    }
+
+    public void posicionFrame() {
+        for (char[] fila : contenido) {
+            for (char pixel : fila) {
+                System.out.print(pixel + " ");
+            }
+            System.out.println();
+        }
     }
 }
