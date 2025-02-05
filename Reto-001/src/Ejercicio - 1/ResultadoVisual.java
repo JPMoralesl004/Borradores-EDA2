3c297aaec7b73609fd8f7b31a4ff06f67af7cb17
JP <144849667+JPMoralesl004@users.noreply.github.com>
2025-02-05 13:30:37 +0100

    Update ResultadoVisual.java

--- Reto-001/src/Ejercicio - 1/ResultadoVisual.java
+++ Reto-001/src/Ejercicio - 1/ResultadoVisual.java
@@ -1,36 +1,21 @@
 public class ResultadoVisual {
+    @SuppressWarnings("unused")
     private char[][] contenido;
-    
-    public ResultadoVisual(int width, int height) {
-        contenido = new char[height][width * 2];
+
+    public ResultadoVisual (int ancho, int alto) {
+        contenido = new char[ancho][alto];
     }
-    
-    public void combinar(Frame1 f1, Frame2 f2) {
-        char[][] c1 = f1.getContenido();
-        char[][] c2 = f2.getContenido();
-        
-        for (int i = 0; i < c1.length; i++) {
-            for (int j = 0; j < c1[0].length; j++) {
-                contenido[i][j * 2] = c1[i][j];
-                contenido[i][j * 2 + 1] = c2[i][j];
+
+    public void combinar(Frame1 frame1, Frame2 frame2) {
+        for (int i = 0; i < frame1.contenido.length; i++) {
+            for (int j = 0; j < frame1.contenido[0].length; j++) {
+                System.out.print(frame1.contenido[i][j] + " ");
             }
+            System.out.print(" ");
+            for (int j = 0; j < frame2.contenido[0].length; j++) {
+                System.out.print(frame2.contenido[i][j] + " ");
+            }
+            System.out.println();
         }
-    }
-    
-    public void mostrarResultado() {
-        for (char[] row : contenido) {
-            System.out.println(new String(row));
-        }
-    }
-    
-    public static void main(String[] args) {
-        int width = 7, height = 3;
-        
-        Frame1 f1 = new Frame1(width, height);
-        Frame2 f2 = new Frame2(width, height);
-        
-        ResultadoVisual rv = new ResultadoVisual(width, height);
-        rv.combinar(f1, f2);
-        rv.mostrarResultado();
     }
 }
